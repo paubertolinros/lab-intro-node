f7708a714d796472ce67f41af75c88989319f2c7
Pau Bertolin <paulaberos@gmail.com>
2022-12-10 14:11:31 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if ( pos >= this.length ) {
+      throw new Error('OutOfBounds')
+    };
+    return this.items[pos]  
+  };
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.max.apply(Math, this.items)
+    };
+  };
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.min.apply(Math, this.items)
+    };
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0
+    } else {
+      return this.items.reduce((acc, curr)=> acc + curr)
+    };
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items.reduce((acc, curr)=> acc + curr) / this.items.length
+    };
+  }
 }
 
 module.exports = SortedList;
